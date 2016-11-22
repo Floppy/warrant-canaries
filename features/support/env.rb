bf335bfd1b0e1eb77ab5d85e3b630aeb56f5ed0d
James Smith <james@floppy.org.uk>
2016-11-22 11:33:58 +0000

    ignore JS errors

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,6 +2,11 @@ require 'capybara/cucumber'
 require 'capybara/poltergeist'
 require 'rspec/expectations'
 
-Capybara.run_server = false
-Capybara.default_driver = :poltergeist
 Capybara.javascript_driver = :poltergeist
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, {js_errors: false})
+end
+Capybara.current_driver = :poltergeist
+Capybara.default_driver = :poltergeist
+
+Capybara.run_server = false
